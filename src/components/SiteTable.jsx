433fba2a73810ff7821f59c17faa38c43cfb5573
Gayathri-PR <94344247+Gayathri-PR@users.noreply.github.com>
2022-11-17 22:29:08 +0530

    Add files via upload

---
+++ src/components/SiteTable.jsx
@@ -0,0 +1,75 @@
+import React,{useState} from 'react'
+import 'antd/dist/antd.css'
+import {Table} from "antd"
+import Navbar from './Navbar'
+import {List} from '../assets/JS Data/List'
+
+const SiteTable = () => {
+  const [Data, setData] = useState(List); 
+  const columns = [
+    {
+      title: 'Sr. No',
+      dataIndex: 'sr',
+      key: 'sr',
+    },
+    {
+      title: 'Name of Monument/Site',
+      dataIndex: 'site',
+      key: 'site',
+    },
+    {
+      title: 'Village',
+      dataIndex: 'village',
+      key: 'village',
+    },
+    {
+        title: 'Taluka',
+        dataIndex: 'taluka',
+        key: 'taluka',
+    },
+    {
+        title: 'Survey No.',
+        dataIndex: 'sno',
+        key: 'sno',
+    },
+    {
+        title: 'Sub-Div',
+        dataIndex: 'subdiv',
+        key: 'subdiv',
+    },
+    {
+        title: 'Ownership',
+        dataIndex: 'own',
+        key: 'own',
+    },
+    {
+        title: 'Police Station',
+        dataIndex: 'ps',
+        key: 'ps',
+    },
+  ];
+  return (
+    //console.log("bdhsd",Data),
+    <>
+    <div style={{backgroundColor:'#ccc'}}>
+    <Navbar/>
+    <>
+    <div className="app"> 
+    <div className="table"> 
+    <Table dataSource={Data} columns={columns} pagination={false} /> 
+    </div> 
+    </div> 
+    </>
+    {/* <Landing/> */}
+    {/* <Featured /
+    > */}
+    {/* <Protected /> */}
+    {/* <Landing/> */}
+    {/* <Card/> */}
+    {/* <About /> */}
+    </div>
+    </>
+  )
+}
+
+export default SiteTable
