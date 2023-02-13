cbb9a11ccda25c1fe048563f20575dff72251ed3
nidhi <nidhiiikamat99@gmail.com>
2023-02-13 13:57:55 +0530

    geo links

--- src/assets/JS Data/Featured.js
+++ src/assets/JS Data/Featured.js
@@ -4,42 +4,47 @@ export default[{
     Title:"Ruins of Brahmapuri, Ella in Tiswadi",
     Description:`This Site in Ella (Old Goa) \n was the settlement of learned Brahmins from ancient times called as ‘Brahmapuri’.  Shiva under the name “Gomanteshwar” is venerated here.  The original temple is reported to have been constructed in the 14th Century C.E.  It is believed that the ancient holy place of worship of Gomanteshwar was destroyed by the Bahamani Kings and the same was rebuilt by Madhava Mantri a minister of the Vijayanagar Empire.  A tank near the temple is known as Madhava Tirth.  This temple was again destroyed by the Portuguese during their conquest of Goa in early 16th Century C.E.   Now Madhav Tirth and a tank exist at this site alongwith a recently built temple.`
     ,Map:"https://goo.gl/maps/E8apDQeW3owtuJAL6",
-    Geo:""
+    Geo:"https://earth.app.goo.gl/?apn=com.google.earth&isi=293622097&ius=googleearth&link=https%3a%2f%2fearth.google.com%2fweb%2fsearch%2fGomanteshwar%2bTemple,%2bUnnamed%2bRoad,%2bElla,%2bGoa%2f%4015.48151075,73.93197248,219.21697757a,26544.62797105d,35y,0.87130312h,2.22455295t,0r%2fdata%3dCpcBGm0SZwolMHgzYmJmYmYzNzFlMTA1NTUzOjB4ZjdlMDg0ZGU3MmEyNWFlYRl_69Sm-_ouQCGMiSiBqHpSQCosR29tYW50ZXNod2FyIFRlbXBsZSwgVW5uYW1lZCBSb2FkLCBFbGxhLCBHb2EYAiABIiYKJAlHDG9g-LcuQBHnfzJZkqEuQBlBENyDaY1SQCGwIftPEYxSQCgC"
 },
 {
     Id:"2",
     Title:"Fortress of Colvale, Colvale in Bardez ",
     Description:"Fort of Colvale, also known as St. Minguel Fort is located on the northern border of the Bardez Taluka, on the left bank of river Colvale.  This is complex of four forts namely St. Thom’e de Colvalle, Fort of St. Minguel Thivim, Nossa Senhora de Assumpsao de Thivim and Nossa Senhora de Livramento de Thivim in an area of about approximate 4.5 km in Colvale-Tivim region from Colvale upto the hill of Tivim all four of them were built in one line at different times for strategic reasons.\n Fort of Colvale was constructed by the Portuguese in 1635 A.D during the tenure of Viceroy Conde de Linhares as a barrier against the inroads and to safeguard the region from the attack of the Marathas and other enemies.  Year 1667 witnessed the battle between Shivaji and Portuguese at this Fort.  It was taken by the Bhonsles and recaptured by the Marquis de Lourical and after 17th century it was neglected and went to ruin.  Chapel of St. Minguel is in the vicinity of fort complex."
     ,Map:"https://maps.app.goo.gl/D5rGjFELfpkZQ6L88",
-    Geo:""
+    Geo:"https://earth.app.goo.gl/?apn=com.google.earth&isi=293622097&ius=googleearth&link=https%3a%2f%2fearth.google.com%2fweb%2fsearch%2ffortress%2bof%2bcolvale%2f%4015.6349385,73.850622,27.35699756a,2324.25531833d,35y,244.13839335h,45t,0r%2fdata%3dCn4aVBJOCiUweDNiYmY5NGFlZmQxMjc5Yjk6MHg5ZWM0OTgzNDlkMzdkZjQ0GeBLPPslRS9AIa87gsBvdlJAKhNmb3J0cmVzcyBvZiBjb2x2YWxlGAIgASImCiQJ9wiTBZusLkARLckvXghcLkAZFmVhMfyNUkAh13xED5d8UkAoAg"
+
 },
 {
     Id:"3",
     Title:"Site of Kaivailyamath, Consua in Mormugao ",
     Description:"This is a single celled lateritic rock cut cave of early medieval period, in Consua village of Mormugao taluka.  This cave has a square Yoni-Pitha. The Linga from this cave was detached from the Yoni-Pitha and was lying in a pond in the vicinity of the cave.  Subsequently the Linga from this cave was acquired by the State Museum of Goa in 1985. Many scholars are of the opinion that this is one of the earliest Lingas from Goa."
     ,Map:"https://maps.app.goo.gl/e6N2CQ5oBbofSPzX9",
-    Geo:""
+    Geo:"https://earth.app.goo.gl/?apn=com.google.earth&isi=293622097&ius=googleearth&link=https%3a%2f%2fearth.google.com%2fweb%2fsearch%2fSite%2bof%2bKaivalyamath%2bat%2bConsua,%2bKesarvale,%2bGoa%2f%4015.37229381,73.92046225,44.59851148a,15119.20028736d,35y,0h,22.02348656t,0r%2fdata%3dCpkBGm8SaQolMHgzYmJmYjdkYzYxZjc1OTkzOjB4NGUyM2RiODE1YWFlMTY1YRkX1o13R74uQCHGhJhLKntSQCouU2l0ZSBvZiBLYWl2YWx5YW1hdGggYXQgQ29uc3VhLCBLZXNhcnZhbGUsIEdvYRgCIAEiJgokCWWdEUAw-C5AEap1F4tyjS5AGakkiMMVe1JAIQp9yaxRdFJAKAI"
+
 },
 {
     Id:"4",
     Title:"Site of Temple of Saptakoteswara, Naroa, Diwadi in Tiswadi.",
     Description:"Historical records suggest that this site of the temple of Saptakoteshwar located at Naroa in Diwar existed during the Kadamba period (10th -14th Century C.E).  This deity was the family deity of the Kadamba rulers.  Huge tank in front was known as ‘Koti-Tirth’.  The Kadamba kings patronized this deity and popularized it in their coins and copper plates.  During the Bahamani rule (1355 -66) this temple was destroyed.  It was in 1391 A.D, that Madhava Mantri, Minister of Vijayanagar kingdom re-established the temple.  In 1540 the temple was completely destroyed by the Portuguese.  By 1538 the deity was shifted across the river of Naroa in Hindale (present day Narve village) village of Bicholim Taluka."
     ,Map:"https://maps.app.goo.gl/5tKapnEteSRJQbXg7",
-    Geo:""
+    Geo:"https://earth.app.goo.gl/?apn=com.google.earth&isi=293622097&ius=googleearth&link=https%3a%2f%2fearth.google.com%2fweb%2fsearch%2fSaptakoteshwar%2bTemple,%2bNaroa,%2bGoa%2f%4015.53061259,73.92574502,2.2279561a,2833.1204645d,35y,352.93363056h,13.15391635t,0r%2fdata%3dCowBGmISXAolMHgzYmJmYmYyNzZiNzM5ZDMxOjB4MjQzODNkNWU3YTYwODQxYxmfKpWm9w8vQCGPV3szRXtSQCohU2FwdGFrb3Rlc2h3YXIgVGVtcGxlLCBOYXJvYSwgR29hGAIgASImCiQJ63L1YdAGL0ART2DiRTT9LkAZgD6XLXF6UkAhYA69-tx5UkAoAg"
+
 },
 {
     Id:"5",
     Title:"Church of Reis Magos, Reis Magos in Bardez",
     Description:"This Royal Church was erected in the 16th century.  It is dedicated to the Magi kings namely Gasper, Baltazar and Melchior; it was an important institution for the Portuguese.  Each new Viceroy who arrived in Goa would disembark here before entering the capital at Velha Goa (today’s Old Goa).  Viceroys who died in Goa were buried here as can be seen from the Commemorative slabs.  The feast of Reis Magos which is popularly known as the ‘Three Kings Feast’ is celebrated here on 6th January every year, is a major event in the religious calendar of Goa. "
     ,Map:"https://maps.app.goo.gl/WaRqeHB2rXXHnB9C7",
-    Geo:""
+    Geo:"https://earth.app.goo.gl/?apn=com.google.earth&isi=293622097&ius=googleearth&link=https%3a%2f%2fearth.google.com%2fweb%2fsearch%2fReis%2bmagos%2bfort%2b,%2bGoa%2f%4015.49475766,73.80792688,-0.18829295a,1558.22295897d,35y,210.54852452h,0t,0r%2fdata%3dCmoaQBI6CiUweDNiYmZjMWQzNjU0ZjMyMWQ6MHg0YmU0YWE0M2U2YzBhZTM0KhFSZWlzIE1hZ29zIENodXJjaBgCIAEiJgokCZdmoJP97jRAEdhWoPTLSDRAGaLt6FtuAFRAIUUurCqx6lNA"
+
 },
 {
     Id:"6",
     Title:"Site of Narayandev at Vichundre, Sanguem.",
     Description:"Archaeological ruins, at this site, display two plinth remnants of temples, roughly dated to the early medieval period, on stylistic basis; one dedicated to Shri Narayandev & the other to Shree Mahishasurmardini.  These temples built of laterite and granite stone, provide an insight into the past human activities, in this region, and are valuable sources of cultural evolution in Goa.\nThe image of Shri Narayandev, in standing pose, in the “Garbhagriha” (sanctum sanctorum) of one temple plinth, has a “Prabhavall” (arch), carved with the ten incarnations of Lord Vishnu (Dashavatar).  A “Garuda” (eagle) sculpture is found in the “Sabhamandap” (hall).  The plinth of the temple is decorated with diamond and floral patterns."
     ,Map:"https://maps.app.goo.gl/hGdEm27i6mkLKayr8",
-    Geo:""
+    Geo:"https://earth.app.goo.gl/?apn=com.google.earth&isi=293622097&ius=googleearth&link=https%3a%2f%2fearth.google.com%2fweb%2fsearch%2fsite%2bof%2bNarayandev%2b,%2bsanguem%2f%4015.1046492,74.18585294,85.43131507a,49267.85776348d,35y,0h,21.28126865t,0r%2fdata%3dCocBGl0SVwolMHgzYmJlNTY2YWM0MTI5ODk3OjB4OTljYzM1Y2ZmYzU1NGRhMBkhGKzdGzQuQCEuY_mpYIxSQCocc2l0ZSBvZiBOYXJheWFuZGV2ICwgc2FuZ3VlbRgCIAEiJgokCe1bHjVFoS5AEQWHxFS4KS5AGZHCxAq1glJAIZLV2bOgdVJAKAI"
+
 },
 {
     Id:"7",
@@ -67,27 +72,31 @@ export default[{
     Title:"Site of Ramnath Temple, Loutolim in Salcete",
     Description:"Here existed a magnificent temple of Shri Ramnath prior to its destruction by the Portuguese alongwith other temples in 1560. Shree Ramnath was worshipped in the form of Linga, which was shifted to Ramnathi at Ponda at the time of destruction.  Today only the small shrine dedicated to Shri Ramnath stands as a testimony of this place.\n Archival records preserved in Directorate of Archives and Archaeology mention the details of the site in 16th Century."
     ,Map:"https://maps.app.goo.gl/X2ekE6aBd3XKL9Ew7",
-    Geo:""
+     Geo:"https://earth.app.goo.gl/?apn=com.google.earth&isi=293622097&ius=googleearth&link=https%3a%2f%2fearth.google.com%2fweb%2fsearch%2ffort%2bSt%2bEstevam%2bIsland,%2bGoa%2f%4015.53590154,73.95363681,35.74851925a,6844.24737119d,35y,0.00294085h,26.9195862t,0r%2fdata%3dCoYBGlwSVgolMHgzYmJmYmRjMWYwNDhjYTFmOjB4NDEwZjU0OGM3MjllNTA4ZBkthrwveBIvQCFW2j3UEX1SQCobZm9ydCBTdCBFc3RldmFtIElzbGFuZCwgR29hGAIgASImCiQJlwNsQnsrL0ARRA7XAz72LkAZ8JTDQhV-UkAhwRny5LR6UkAoAg"
+
 },
 {
     Id:"11",
     Title:"Cave at Ishwarbhat, Khandepar in Ponda.",
     Description:"Situated on the left bank of Khandepar River about 3 km from Khandepar cave.  This is a monolithic single cell lateritic rock-cut cave of early medeival period.  Inside the cave Shiva Linga is placed on Yonipitha. Scholars believe that this cave has architectural similarities with Khandepar Caves and the caves at Narve and probably contemporary in time."
     ,Map:"https://maps.app.goo.gl/WZyAg4fiMqwyWZLbA",
-    Geo:""
+    Geo:"https://earth.app.goo.gl/?apn=com.google.earth&isi=293622097&ius=googleearth&link=https%3a%2f%2fearth.google.com%2fweb%2fsearch%2fcave%2bat%2bishwarbhat%2f%4015.45209583,74.0389399,7.72994392a,4891.49984191d,35y,332.34932255h,11.17530915t,0r%2fdata%3dCn0aUxJNCiUweDNiYmZhNTg4YmEwN2U0MTM6MHg2NTFmYzIxNGJmYjVmMmQ4GcGHt3MG4y5AIU64sp-gglJAKhJjYXZlIGF0IGlzaHdhcmJoYXQYAiABIiYKJAkV4cPD_fouQBFp0dzLhu8uQBmcx1dpwoJSQCF6xkIRM35SQCgC"
+
 },
 {
     Id:"12",
     Title:"Fortress of Colvale, Colvale in Bardez ",
     Description:"Fort of Colvale, also known as St. Minguel Fort is located on the northern border of the Bardez Taluka, on the left bank of river Colvale.  This is complex of four forts namely St. Thom’e de Colvalle, Fort of St. Minguel Thivim, Nossa Senhora de Assumpsao de Thivim and Nossa Senhora de Livramento de Thivim in an area of about approximate 4.5 km in Colvale-Tivim region from Colvale upto the hill of Tivim all four of them were built in one line at different times for strategic reasons.\n Fort of Colvale was constructed by the Portuguese in 1635 A.D during the tenure of Viceroy Conde de Linhares as a barrier against the inroads and to safeguard the region from the attack of the Marathas and other enemies.  Year 1667 witnessed the battle between Shivaji and Portuguese at this Fort.  It was taken by the Bhonsles and recaptured by the Marquis de Lourical and after 17th century it was neglected and went to ruin.  Chapel of St. Minguel is in the vicinity of fort complex."
     ,Map:"https://maps.app.goo.gl/D5rGjFELfpkZQ6L88",
-    Geo:""
+    Geo:"https://earth.app.goo.gl/?apn=com.google.earth&isi=293622097&ius=googleearth&link=https%3a%2f%2fearth.google.com%2fweb%2fsearch%2ffortress%2bof%2bcolvale%2f%4015.6349385,73.850622,27.35699756a,2324.25531833d,35y,244.13839335h,45t,0r%2fdata%3dCn4aVBJOCiUweDNiYmY5NGFlZmQxMjc5Yjk6MHg5ZWM0OTgzNDlkMzdkZjQ0GeBLPPslRS9AIa87gsBvdlJAKhNmb3J0cmVzcyBvZiBjb2x2YWxlGAIgASImCiQJ9wiTBZusLkARLckvXghcLkAZFmVhMfyNUkAh13xED5d8UkAoAg"
+
 },
 {
     Id:"13",
     Title:"Cave at Aquem, Margao in Salcete.",
     Description:"This is a lateritic man-made cave of early medieval period. It is popularly known as ‘Pandava Ovreo’ (Cave of Pandava).  It has 3 openings and 2 large rooms; with pillars inside.  This rock-cut cave is rectangular in shape and is divided into 2 chambers.  On both the sides of the northern door there are simple windows (Gavaksha).  Inner chamber has peg holes and niches.\n The Chapel located next to this cave is popularly known as ‘Pandava Chapel’ after this cave (Pandava Cave)."
     ,Map:"https://maps.app.goo.gl/ndjXSPWYEkJswUtM8",
-    Geo:""
+    Geo:"https://earth.app.goo.gl/?apn=com.google.earth&isi=293622097&ius=googleearth&link=https%3a%2f%2fearth.google.com%2fweb%2fsearch%2fcave%2bat%2baquem%2f%4015.2725405,73.9707957,19.51948842a,2329.1853661d,35y,-143.63671022h,45t,0r%2fdata%3dCngaThJICiUweDNiYmZiM2M1ZjM0ODRhNjk6MHhhZjI1YjFlM2U4NDdmZTNhGcWrrG2Kiy5AITymSYQhflJAKg1jYXZlIGF0IGFxdWVtGAIgASImCiQJeuf3xrOgLkARc21P6bGYLkAZv6eqAwSBUkAhmxDwf5B_UkA"
+ 
 },
 ]
