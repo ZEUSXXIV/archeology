3f5917d87e0658503beab96f16bbf397d6a67a45
nidhi <nidhiiikamat99@gmail.com>
2023-03-11 16:23:35 +0530

    Footer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Landing from './components/Home/Landing'
 import Navbar from './components/Navbar'
 import Protected from './pages/Protected'
 import Featured from './pages/Featured'
+import SimpleReactFooter from "simple-react-footer";
 import About from './pages/About'
 import Body from './components/Home/Body'
 import './styles/global.css'
@@ -14,7 +15,57 @@ import MonumentList from './pages/MonumentList'
 import Gallery from './pages/Gallery'
 import SubGallery from './components/SubGallery'
 
-
+const description = "According to wikipedia, the cat (Felis catus) is a domestic species of small carnivorous mammal. It is the only domesticated species in the family Felidae and is often referred to as the domestic cat to distinguish it from the wild members of the family. A cat can either be a house cat, a farm cat or a feral cat; the latter ranges freely and avoids human contact.";
+const title = "Cats";
+const columns = [
+  {
+      title: "Resources",
+      resources: [
+          {
+              name: "About",
+              link: "/about"
+          },
+          {
+              name: "Careers",
+              link: "/careers"
+          },
+          {
+              name: "Contact",
+              link: "/contact"
+          },
+          {
+              name: "Admin",
+              link: "/admin"
+          }
+      ]
+  },
+  {
+      title: "Legal",
+      resources: [
+          {
+              name: "Privacy",
+              link: "/privacy"
+          },
+          {
+              name: "Terms",
+              link: "/terms"
+          }
+      ]
+  },
+  {
+      title: "Visit",
+      resources: [
+          {
+              name: "Locations",
+              link: "/locations"
+          },
+          {
+              name: "Culture",
+              link: "/culture"
+          }
+      ]
+  }
+];
 
 const App = () => {
   return (
@@ -37,9 +88,30 @@ const App = () => {
           <Route element={<About/>} exact path='/about-us' />
           <Route element={<MonumentList/>} exact path='/monument-list' />
           <Route element={<Gallery/>} exact path='/gallery' />
-          <Route element={<SubGallery/>} exact path='/subgallery' />
+          <Route element={<SubGallery/>} exact path='/subgallery/:id' />
         </Routes>
       </Router>
+   
+
+ 
+  <SimpleReactFooter 
+    description={description} 
+    title={title}
+    columns={columns}
+    linkedin="fluffy_cat_on_linkedin"
+    facebook="fluffy_cat_on_fb"
+    twitter="fluffy_cat_on_twitter"
+    instagram="fluffy_cat_live"
+    youtube="UCFt6TSF464J8K82xeA?"
+    pinterest="fluffy_cats_collections"
+    copyright="black"
+    iconColor="white"
+    backgroundColor="#24262b"
+    fontColor="white"
+    copyrightColor="darkgrey"
+    
+ />
+
     
     {/* <Featured />
     <Protected />
