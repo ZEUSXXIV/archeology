038ed9cd3d23fdfd4ab45146fbaac711860fcfa9
ZEUSXXIV <navzeus192021@gmail.com>
2023-06-07 11:41:49 +0530

    some gallery shit

--- src/Pages/Gallery.jsx
+++ src/Pages/Gallery.jsx
@@ -1,12 +1,31 @@
 import React from "react";
-import { useState} from "react";
+import { useState,useEffect} from "react";
 import pics from "../assets/JS Data/Gallery";
 import Gallery_component from "../components/Gallery_component";
 import { Typography } from "antd";
 import Styles from "../styles/Gallery.module.css"
+import axios from "axios";
 
 const Gallery = () => {
         const [activeItem, setActiveItem] = useState(null);
+        const [data,setData]=useState([])
+        useEffect(() => {
+          axios.get("http://localhost:5000/api/v1/gallery").then((res, err) => {
+            if (err) {
+              console.log("err", err);
+            }
+      
+            console.log(res.data);
+            let temp=res.data.filter((item)=> {
+              if(item.main_img === "")
+              return item
+            })
+
+            console.log(temp)
+            setData(temp);
+          });
+        }, []);
+      
         return(
   <>
   <div style={{background:"rgb(255,237,204)"}}>
@@ -19,17 +38,19 @@ const Gallery = () => {
         justifyContent: "center",
         alignItems: "center",
       }}>
-    {pics.map((item) => (
-            <div key={item.Title}  onClick={() => {
+    {data.map((item) => (
+      < >
+            {item.main_img && <div onClick={() => {
                 setActiveItem(item);
-                window.location.href="/subgallery/"+item.Id;
+                window.location.href="/subgallery/"+item.monument_id;
               }}>{
               <Gallery_component
-                Title={item.Title}
-                mainImg={item.mainImg}
-                key={item.Id}
+                Title={item.site}
+                mainImg={item.main_img}
+                key={item.monument_id}
               />}
-            </div>
+            </div>}
+            </>
           ))}
       
         </div>
--- src/components/SubGallery.jsx
+++ src/components/SubGallery.jsx
@@ -1,29 +1,55 @@
 import MainImage from "../assets/main.jpg";
-import Main1 from "../assets/main1.jpg"
-import ImageGallery from 'react-image-gallery';
+import Main1 from "../assets/main1.jpg";
+import ImageGallery from "react-image-gallery";
 import { PhotoAlbum } from "react-photo-album";
 import Styles from "../styles/SubGallery.module.css";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
 const photos = [
-  { src: require('../assets/main.jpg'), width:1800, height: 1600 },
-  { src:  require('../assets/main1.jpg'), width:1800, height:1780 },
-  { src:  require('../assets/try.jpg'), width: 2000, height: 1900 },
-  { src:  require('../assets/main1.jpg'), width: 1600, height: 900 },
-  { src:  require('../assets/main1.jpg'), width: 1600, height: 900 },
-  { src:  require('../assets/main1.jpg'), width: 1600, height: 900 },
-  { src: require('../assets/main.jpg'), width:1800, height: 1600 },
-  
-
+  { src: require("../assets/main.jpg"), width: 1800, height: 1600 },
+  { src: require("../assets/main1.jpg"), width: 1800, height: 1780 },
+  { src: require("../assets/try.jpg"), width: 2000, height: 1900 },
+  { src: require("../assets/main1.jpg"), width: 1600, height: 900 },
+  { src: require("../assets/main1.jpg"), width: 1600, height: 900 },
+  { src: require("../assets/main1.jpg"), width: 1600, height: 900 },
+  { src: require("../assets/main.jpg"), width: 1800, height: 1600 },
 ];
 
-
 export default function SubGallery() {
-  
+  const { id } = useParams();
+  const [data, setData] = useState([]);
+  useEffect(() => {
+
+    console.log("Id", id)
+
+    axios.get(`http://localhost:5000/api/v1/gallery/${id}`).then((res, err) => {
+      if (err) {
+        console.log("err", err);
+      }
+
+      console.log("lewl",res.data);
+
+      let temp = []
+      
+      temp.push( { src: res.data[0]?.main_img, width: 1800, height: 1600 })
+      
+
+      res.data[0]?.subgallery?.map((item) =>{
+        temp.push( { src: item, width: 1800, height: 1600 })
+      } )
+
+     
+      setData(temp);
+    });
+  }, []);
   return (
-    
-   <>
-   <div className={`${Styles.container}`} >
-   <PhotoAlbum layout="rows" photos={photos}/>
-   </div>
-   </>
+    <>
+      <div className={`${Styles.container}`}>
+        <PhotoAlbum layout="rows" photos={data}/>
+        {/* {data?.subgallery?.map((item) => <img src={item} alt="fuck off" />
+        )} */}
+      </div>
+    </>
   );
 }
